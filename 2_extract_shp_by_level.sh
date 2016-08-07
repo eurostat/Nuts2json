7ef97cffbeff0763b53622876dd586ba8c9becd2
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-07 19:31:51 +0200

    decompose BN

--- 2_extract_shp_by_level.sh
+++ 2_extract_shp_by_level.sh
@@ -3,12 +3,15 @@
 for proj in "etrs89" "laea" "wm"
 do
     echo "Projection "$proj
-    dir="tmp/"$proj"/RGbylevel"
-    mkdir -p $dir
+    dirRG="tmp/"$proj"/RGbylevel"
+    dirBN="tmp/"$proj"/BNbylevel"
+    mkdir -p $dirRG
+    mkdir -p $dirBN
 
     for level in 0 1 2 3
     do
         echo "   Extract nuts level "$level
-        ogr2ogr -overwrite -where STAT_LEVL_=$level $dir"/RG_lvl"$level".shp" "tmp/"$proj"/RG.shp"
+        ogr2ogr -overwrite -where STAT_LEVL_=$level $dirRG"/RG_lvl"$level".shp" "tmp/"$proj"/RG.shp"
+        ogr2ogr -overwrite -sql "SELECT * FROM BN WHERE STAT_LEVL_<="$level $dirBN"/BN_lvl"$level".shp" "tmp/"$proj"/BN.shp"
     done
 done
