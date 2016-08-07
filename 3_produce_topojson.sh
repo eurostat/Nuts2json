ecb2be0efc36864d4315618d2e64e8efbcc43978
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-07 20:54:12 +0200

    new versions with BN and RG

--- 3_produce_topojson.sh
+++ 3_produce_topojson.sh
@@ -8,5 +8,6 @@ do
         echo "Produce topojson for level $level and projection $proj"
         topojson -o "json/topojson/RG_lvl"$level"_"$proj".json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp"
         topojson -o "json/topojson/BN_lvl"$level"_"$proj".json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp"
+        topojson -o "json/topojson/RG_BN_lvl"$level"_"$proj".json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp"
     done
 done
