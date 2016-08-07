6e50fd1839c927e575651f28069d1bd78a039b95
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-07 21:28:57 +0200

    test simplification

--- 3_produce_topojson.sh
+++ 3_produce_topojson.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-mkdir -p json/topojson
-for proj in "etrs89" "laea" "wm"
+for proj in "etrs89" #"laea" #"wm"
 do
-    for level in 0 1 2 3
+    mkdir -p "json/topojson/"$proj
+    for level in 0 #1 2 3
     do
-        echo "Produce topojson for level $level and projection $proj"
-        topojson -o "json/topojson/RG_lvl"$level"_"$proj".json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp"
-        topojson -o "json/topojson/BN_lvl"$level"_"$proj".json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp"
-        topojson -o "json/topojson/RG_BN_lvl"$level"_"$proj".json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp"
+        for scaleM in 3 10 30 60
+        do
+            res=$((2/10*$scaleM*1000))
+            echo "Produce topojson for level "$level" - projection "$proj" - scale 1:"$scaleM"M - resolution="$res
+            topojson -o "json/topojson/"$proj"/RG_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" -s res #--id-property NUTS_ID
+            topojson -o "json/topojson/"$proj"/BN_lvl"$level"_"$scaleM"M.json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
+            topojson -o "json/topojson/"$proj"/RG_BN_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
+        done
     done
 done
