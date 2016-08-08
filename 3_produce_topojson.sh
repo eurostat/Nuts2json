3a2aec45da236eaab7210f042417f52aca022067
gaffuri <julien.gaffuri@gmail.com>
2016-08-08 08:56:31 +0200

    Update 3_produce_topojson.sh

--- 3_produce_topojson.sh
+++ 3_produce_topojson.sh
@@ -2,21 +2,28 @@
 
 for proj in "laea" "wm" #"etrs89"
 do
-    mkdir -p "json/topojson/"$proj
     for level in 0 1 2 3
     do
         for scaleM in 3 10 30 60
         do
+            #make directory
+            dir="json/topojson/"$proj"/"$scaleM"M"
+            mkdir -p dir
+
+            #compute resolution
+            res=0
             if [ $proj = "etrs89" ]
                 #then res=$((200*$scaleM))
                 then res=$(echo "$scaleM / 60" | bc -l)
                 else res=$((200*$scaleM))
             fi
+
             #qua=$res
+
             echo "   Produce topojson - lvl"$level" - "$proj" - 1:"$scaleM"M - res="$res   #" - qua="$qua
-            topojson -o "json/topojson/"$proj"/RG_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" -s res #-q qua #--id-property NUTS_ID
-            topojson -o "json/topojson/"$proj"/BN_lvl"$level"_"$scaleM"M.json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
-            topojson -o "json/topojson/"$proj"/RG_BN_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
+            topojson -o $dir"/RG_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" -s res #-q qua #--id-property NUTS_ID
+            topojson -o $dir"/BN_lvl"$level"_"$scaleM"M.json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
+            topojson -o $dir"/RG_BN_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
         done
     done
 done
