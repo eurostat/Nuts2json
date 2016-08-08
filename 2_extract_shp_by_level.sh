3832792c18b40a0ab90b7e5106507b591737e814
gaffuri <julien.gaffuri@gmail.com>
2016-08-08 08:48:37 +0200

    Update 2_extract_shp_by_level.sh

--- 2_extract_shp_by_level.sh
+++ 2_extract_shp_by_level.sh
@@ -2,7 +2,9 @@
 
 for proj in "etrs89" "laea" "wm"
 do
-    echo "Projection "$proj
+    echo "Extract nuts level - "$proj
+
+    #make folders
     dirRG="tmp/"$proj"/RGbylevel"
     dirBN="tmp/"$proj"/BNbylevel"
     mkdir -p $dirRG
@@ -10,8 +12,9 @@ do
 
     for level in 0 1 2 3
     do
-        echo "   Extract nuts level "$level
+        echo "   RG level "$level
         ogr2ogr -overwrite -where STAT_LEVL_=$level $dirRG"/RG_lvl"$level".shp" "tmp/"$proj"/RG.shp"
+        echo "   BN level "$level
         ogr2ogr -overwrite -sql "SELECT * FROM BN WHERE STAT_LEVL_<="$level $dirBN"/BN_lvl"$level".shp" "tmp/"$proj"/BN.shp"
     done
 done
