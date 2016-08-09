cff5c0b7562803fa80224fe31a079b2714304712
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-09 21:24:41 +0200

    new version

--- 3_produce_topojson.sh
+++ 3_produce_topojson.sh
@@ -1,33 +1,49 @@
 #!/usr/bin/env bash
 
-for proj in "etrs89" #"laea" "wm"
+margin=20
+for size in 300 500 700 1000 1200
 do
-    for level in 0 #1 2 3
+    for level in 0 1 2 3
     do
-        for scaleM in 3 10 30 60
+        for proj in "laea" "wm" "etrs89"
         do
             #make directory
-            dir="json/topojson/"$proj"/"$scaleM"M"
+            dir="json/topojson/"$proj"/"$size"px"
             mkdir -p $dir
 
-            #compute resolution
-            res=0
             if [ $proj = "etrs89" ]
-                #then res=$((200*$scaleM))
-                then res=$(echo "$scaleM / 60" | bc -l)
-                else res=$((200*$scaleM))
-            fi
+            then
 
-            #qua=$res
+            #compute resolution
+            #res=0
+            #if [ $proj = "etrs89" ]
+            #    #then res=$((200*$scaleM))
+            #    then res=$(echo "$scaleM / 60" | bc -l)
+            #    else res=$((200*$scaleM))
+            #fi
 
-            echo "   Produce topojson - lvl"$level" - "$proj" - 1:"$scaleM"M - res="$res   #" - qua="$qua
+            else
+
+            echo "   Produce topojson - lvl"$level" - "$proj" - "$size"px"
             topojson -o \
-                $dir"/RG_lvl"$level"_"$scaleM"M.json" \
+                $dir"/RG_lvl"$level".json" \
                 "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" \
-                -s 1e-7
-                #-q qua #--id-property NUTS_ID  \
-            #topojson -o $dir"/BN_lvl"$level"_"$scaleM"M.json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
-            #topojson -o $dir"/RG_BN_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
+                --width $size --height $size --margin $margin \
+                -s 1
+                #-s 1e-7
+                #--id-property NUTS_ID  \
+            topojson -o \
+                $dir"/BN_lvl"$level".json" \
+                "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" \
+                --width $size --height $size --margin $margin \
+                -s 1
+            topojson -o \
+                $dir"/RG_BN_lvl"$level".json" \
+                "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" \
+                "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" \
+                --width $size --height $size --margin $margin \
+                -s 1
+            fi
         done
     done
 done
