907e35be57ef5c9220b2bb357d9f1fd6b1ef588e
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-09 19:48:10 +0200

    new version

--- 1_project.sh
+++ 1_project.sh
@@ -8,10 +8,22 @@ mkdir -p tmp/laea
 for type in "RG" "BN"
 do
     echo "Project $type"
+
     echo "   to ETRS 89"
-    ogr2ogr -overwrite -f "ESRI Shapefile" "tmp/etrs89/"$type".shp" "shp/NUTS_"$type"_01M_2013.shp" -t_srs EPSG:4258 -s_srs EPSG:4258
+    ogr2ogr -overwrite -f "ESRI Shapefile" \
+        "tmp/etrs89/"$type".shp" \
+        "shp/NUTS_"$type"_01M_2013.shp" \
+        -t_srs EPSG:4258 -s_srs EPSG:4258
+
     echo "   to Web Mercator"
-    ogr2ogr -overwrite -f "ESRI Shapefile" "tmp/wm/"$type".shp" "shp/NUTS_"$type"_01M_2013.shp" -t_srs EPSG:3857 -s_srs EPSG:4258
+    ogr2ogr -overwrite -f "ESRI Shapefile" \
+        "tmp/wm/"$type".shp" \
+        "shp/NUTS_"$type"_01M_2013.shp" \
+        -t_srs EPSG:3857 -s_srs EPSG:4258
+
     echo "   to LAEA"
-    ogr2ogr -overwrite -f "ESRI Shapefile" "tmp/laea/"$type".shp" "shp/NUTS_"$type"_01M_2013.shp" -t_srs EPSG:3035 -s_srs EPSG:4258
+    ogr2ogr -overwrite -f "ESRI Shapefile" \
+        "tmp/laea/"$type".shp" \
+        "shp/NUTS_"$type"_01M_2013.shp" \
+        -t_srs EPSG:3035 -s_srs EPSG:4258
 done
--- 3_produce_topojson.sh
+++ 3_produce_topojson.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-for proj in "laea" "wm" #"etrs89"
+for proj in "etrs89" #"laea" "wm"
 do
-    for level in 0 1 2 3
+    for level in 0 #1 2 3
     do
         for scaleM in 3 10 30 60
         do
             #make directory
             dir="json/topojson/"$proj"/"$scaleM"M"
-            mkdir -p dir
+            mkdir -p $dir
 
             #compute resolution
             res=0
@@ -21,9 +21,13 @@ do
             #qua=$res
 
             echo "   Produce topojson - lvl"$level" - "$proj" - 1:"$scaleM"M - res="$res   #" - qua="$qua
-            topojson -o $dir"/RG_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" -s res #-q qua #--id-property NUTS_ID
-            topojson -o $dir"/BN_lvl"$level"_"$scaleM"M.json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
-            topojson -o $dir"/RG_BN_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
+            topojson -o \
+                $dir"/RG_lvl"$level"_"$scaleM"M.json" \
+                "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" \
+                -s 1e-7
+                #-q qua #--id-property NUTS_ID  \
+            #topojson -o $dir"/BN_lvl"$level"_"$scaleM"M.json" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
+            #topojson -o $dir"/RG_BN_lvl"$level"_"$scaleM"M.json" "nutsrg=tmp/"$proj"/RGbylevel/RG_lvl"$level".shp" "nutsbn=tmp/"$proj"/BNbylevel/BN_lvl"$level".shp" -s $res
         done
     done
 done
