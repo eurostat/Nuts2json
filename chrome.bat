640f162d1416ad9650b917ee8785e9327784edd8
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-08-10 09:46:53 +0200

    cleaning

--- chrome.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files H:\workspace\Nuts2json\overview.html
--- chrome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#google-chrome --allow-access-from-files
-google-chrome --allow-file-access-from-files
-#chromium-browser --disable-web-security
