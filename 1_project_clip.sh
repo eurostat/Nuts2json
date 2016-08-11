1f21dd1608d23e9352a00003c705c04a29cab65c
julien Gaffuri <julien.gaffuri@gmail.com>
2016-08-11 22:47:52 +0200

    new version with clip + info text

--- 1_project_clip.sh
+++ 1_project_clip.sh
@@ -2,6 +2,10 @@
 
 projs=("etrs89" "wm" "laea")
 epsgs=("4258" "3857" "3035")
+xmin=(-12.5 -1490000 2434550)
+ymin=(32.5 3884000 1340330)
+xmax=(46.5 5200000 7512400)
+ymax=(73.9 11690000 5664600)
 
 for pi in ${!projs[@]}
 do
@@ -12,8 +16,14 @@ do
     do
         echo "Project $type to $proj"
         ogr2ogr -overwrite -f "ESRI Shapefile" \
-            "tmp/"$proj"/"$type".shp" \
+            "tmp/"$proj"/"$type"_proj.shp" \
             "shp/NUTS_"$type"_01M_2013.shp" \
             -t_srs EPSG:$epsg -s_srs EPSG:4258
+
+        echo "Clip $type"
+        ogr2ogr -overwrite -f "ESRI Shapefile" \
+            "tmp/"$proj"/"$type".shp" \
+            "tmp/"$proj"/"$type"_proj.shp" \
+            -clipsrc ${xmin[pi]} ${ymin[pi]} ${xmax[pi]} ${ymax[pi]}
     done
 done
--- 2_extract_shp_by_level.sh
+++ 2_extract_shp_by_level.sh
@@ -14,9 +14,10 @@ do
     do
         echo "   RG level "$level
         ogr2ogr -overwrite \
-            -sql "SELECT * FROM RG WHERE STAT_LEVL_="$level" AND NUTS_ID NOT IN ('FRA','FRA1','FRA2','FRA3','FRA4','FRA5','FRA10','FRA20','FRA30','FRA40','FRA50','PT2','PT20','PT200','PT3','PT30','PT300','ES7','ES70','ES701','ES702','ES703','ES704','ES705','ES706','ES707','ES708','ES709')" \
+            -sql "SELECT * FROM RG WHERE STAT_LEVL_="$level \
             $dirRG"/RG_lvl"$level".shp" \
             "tmp/"$proj"/RG.shp"
+            #" AND NUTS_ID NOT IN ('FRA','FRA1','FRA2','FRA3','FRA4','FRA5','FRA10','FRA20','FRA30','FRA40','FRA50','PT2','PT20','PT200','PT3','PT30','PT300','ES7','ES70','ES701','ES702','ES703','ES704','ES705','ES706','ES707','ES708','ES709')"
 
         echo "   BN level "$level
         ogr2ogr -overwrite \
