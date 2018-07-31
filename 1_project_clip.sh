498389bc2b967b329787ceed84864d009ee73684
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-31 18:06:01 +0200

    a

--- 1_project_clip.sh
+++ 1_project_clip.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-years=("2016")
+years=("2013" "2016")
 projs=("etrs89" "wm" "laea")
 epsgs=("4258" "3857" "3035")
 xmin=(-12.5 -1490000 2434550)
@@ -18,21 +18,21 @@ year=${years[pi]}
 mkdir -p "tmp/"$year
 
 
-echo "Country: Clip and filter RG"
+echo "Country $year: Clip and filter RG"
 ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/CNTR_RG_.shp" \
    "shp/"$year"/CNTR_RG_01M_"$year".shp" \
    -sql "SELECT * FROM CNTR_RG_01M_"$year" WHERE CNTR_ID NOT IN ('PT','ES','IE','UK','FR','IS','BE','LU','NL','CH','LI','DE','DK','IT','VA','MT','NO','SE','FI','EE','LV','LT','PL','CZ','SK','AT','SI','HU','HR','RO','BG','TR','EL','CY','MK','ME')" \
    -clipsrc -179 -89 179 89
 
-echo "Country: Join RG attributes"
+echo "Country $year: Join RG attributes"
 ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/CNTR_RG.shp" \
    "tmp/"$year"/CNTR_RG_.shp" \
    -sql "select CNTR_RG_.CNTR_ID as ID, CNTR_AT_"$year".CNTR_NAME as NAME from CNTR_RG_ left join 'shp/"$year"/CNTR_AT_"$year".csv'.CNTR_AT_"$year" on CNTR_RG_.CNTR_ID = CNTR_AT_"$year".CNTR_ID" \
    -clipsrc -179 -89 179 89
 
-echo "Country: Clip and filter BN"
+echo "Country $year: Clip and filter BN"
 ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/CNTR_BN.shp" \
    "shp/"$year"/CNTR_BN_01M_"$year".shp" \
@@ -44,28 +44,28 @@ for pi in ${!projs[@]}
 do
     proj=${projs[pi]}
     epsg=${epsgs[pi]}
-    mkdir -p tmp/"$year"/$proj
+    mkdir -p "tmp/"$year"/"$proj
     for type in "RG" "BN"
     do
-        echo "Project NUTS $type to $proj"
+        echo "NUTS $year: Project $type to $proj"
         ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
             "tmp/"$year"/"$proj"/NUTS_"$type"_proj.shp" \
             "shp/"$year"/NUTS_"$type"_01M_"$year".shp" \
             -t_srs EPSG:$epsg -s_srs EPSG:4258
 
-        echo "Clip NUTS $type $proj"
+        echo "NUTS $year: Clip $type $proj"
         ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
             "tmp/"$year"/"$proj"/NUTS_"$type".shp" \
             "tmp/"$year"/"$proj"/NUTS_"$type"_proj.shp" \
             -clipsrc ${xmin[pi]} ${ymin[pi]} ${xmax[pi]} ${ymax[pi]}
 
-        echo "Project country $type to $proj"
+        echo "Country $year: Project $type to $proj"
         ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
             "tmp/"$year"/"$proj"/CNTR_"$type"_proj.shp" \
             "tmp/"$year"/CNTR_"$type".shp" \
             -t_srs EPSG:$epsg -s_srs EPSG:4258
 
-        echo "Clip country $type $proj"
+        echo "Country $year: Clip $type $proj"
         ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
             "tmp/"$year"/"$proj"/CNTR_"$type".shp" \
             "tmp/"$year"/"$proj"/CNTR_"$type"_proj.shp" \
