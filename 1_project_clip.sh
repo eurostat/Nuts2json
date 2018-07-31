f9ae12552d5b0cc7a1a16888bed05dd9248f18e5
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-31 15:18:00 +0200

    a

--- 1_project_clip.sh
+++ 1_project_clip.sh
@@ -15,7 +15,7 @@ do
 
 year=${years[pi]}
 
-mkdir "tmp/"$year
+mkdir -p "tmp/"$year
 
 echo "Clip and filter country RG"
 ogr2ogr -overwrite -f "ESRI Shapefile" \
@@ -24,21 +24,21 @@ ogr2ogr -overwrite -f "ESRI Shapefile" \
    -sql "SELECT * FROM CNTR_RG_01M_"$year" WHERE CNTR_ID NOT IN ('PT','ES','IE','UK','FR','IS','BE','LU','NL','CH','LI','DE','DK','IT','VA','MT','NO','SE','FI','EE','LV','LT','PL','CZ','SK','AT','SI','HU','HR','RO','BG','TR','EL','CY','MK','ME')" \
    -clipsrc -179 -89 179 89
 
-#echo "Join country RG attributes"
-#ogr2ogr -overwrite -f "ESRI Shapefile" \
-#   "tmp/"$year"/CNTR_RG_01M_"$year"___.shp" \
-#   "tmp/"$year"/CNTR_RG_01M_"$year"_.shp" \
-#   -sql "select CNTR_RG_01M_"$year"_.*, CNTR_AT_"$year".* from CNTR_RG_01M_"$year"_ left join 'shp/"$year"/CNTR_AT_"$year".csv'.CNTR_AT_"$year" on CNTR_RG_01M_"$year"_.CNTR_ID = CNTR_AT_"$year".CNTR_ID" \
-#   -clipsrc -179 -89 179 89
-#
-#echo "Clip and filter country BN and select"
-#ogr2ogr -overwrite -f "ESRI Shapefile" \
-#   "tmp/"$year"/CNTR_BN_01M_"$year"___.shp" \
-#   "shp/"$year"/CNTR_BN_01M_"$year".shp" \
-#   -sql "SELECT * FROM CNTR_BN_01M_"$year" WHERE COAS_FLAG='F' AND OTHR_CNTR_='T'" \
-#   -clipsrc -179 -89 179 89
-#
-#
+echo "Join country RG attributes"
+ogr2ogr -overwrite -f "ESRI Shapefile" \
+   "tmp/"$year"/CNTR_RG_01M_"$year"___.shp" \
+   "tmp/"$year"/CNTR_RG_01M_"$year"_.shp" \
+   -sql "select CNTR_RG_01M_"$year"_.*, CNTR_AT_"$year".* from CNTR_RG_01M_"$year"_ left join 'shp/"$year"/CNTR_AT_"$year".csv'.CNTR_AT_"$year" on CNTR_RG_01M_"$year"_.CNTR_ID = CNTR_AT_"$year".CNTR_ID" \
+   -clipsrc -179 -89 179 89
+
+echo "Clip and filter country BN and select"
+ogr2ogr -overwrite -f "ESRI Shapefile" \
+   "tmp/"$year"/CNTR_BN_01M_"$year"___.shp" \
+   "shp/"$year"/CNTR_BN_01M_"$year".shp" \
+   -sql "SELECT * FROM CNTR_BN_01M_"$year" WHERE COAS_FLAG='F' AND OTHR_CNTR_='T'" \
+   -clipsrc -179 -89 179 89
+
+
 #for pi in ${!projs[@]}
 #do
 #    proj=${projs[pi]}
