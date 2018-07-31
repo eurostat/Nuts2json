41c68e74d2a89d503c3f306d82c5cacb86878f4d
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-31 21:47:26 +0200

    a

--- 2_extract_shp_by_level.sh
+++ 2_extract_shp_by_level.sh
@@ -16,14 +16,14 @@ do
     for level in 0 1 2 3
     do
         echo "   NUTS "$year" RG level "$level
-        ogr2ogr -overwrite \
+        ogr2ogr -overwrite -lco ENCODING=UTF-8 \
             -sql "SELECT * FROM NUTS_RG WHERE LEVL_CODE="$level \
             $dirRG"/RG_lvl"$level".shp" \
             "tmp/"$year"/"$proj"/NUTS_RG.shp"
             #" AND NUTS_ID NOT IN ('FRA','FRA1','FRA2','FRA3','FRA4','FRA5','FRA10','FRA20','FRA30','FRA40','FRA50','PT2','PT20','PT200','PT3','PT30','PT300','ES7','ES70','ES701','ES702','ES703','ES704','ES705','ES706','ES707','ES708','ES709')"
 
         echo "   NUTS "$year" BN level "$level
-        ogr2ogr -overwrite \
+        ogr2ogr -overwrite -lco ENCODING=UTF-8 \
             -sql "SELECT * FROM NUTS_BN WHERE LEVL_CODE<="$level" AND COAS_FLAG <> 'T'" \
             $dirBN"/BN_lvl"$level".shp" \
             "tmp/"$year"/"$proj"/NUTS_BN.shp"
