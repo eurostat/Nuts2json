d41c3ad7229d54f1e0fad100a1b2127315527021
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-04 00:15:13 +0200

    a

--- 999_old_generalise_to_topojson.sh
+++ 999_old_generalise_to_topojson.sh
@@ -36,20 +36,18 @@ do
             else
 
             echo "$year $proj $size $level - Produce topojson"
-
-
-#            topojson -o \
-#                $dir"/"$level".topojson" \
-#                "nutsrg=tmp/"$year"/"$proj"/RGbylevel/RG_lvl"$level".shp" \
-#                "nutsbn=tmp/"$year"/"$proj"/BNbylevel/BN_lvl"$level".shp" \
-#                "cntrg=tmp/"$year"/"$proj"/CNTR_RG.shp" \
-#                "cntbn=tmp/"$year"/"$proj"/CNTR_BN.shp" \
-#                -p id=NUTS_ID,na=name,eu=EU_FLAG,efta=EFTA_FLAG,cc=CC_FLAG,lvl=LEVL_CODE,oth=OTHR_CNTR_,cid=CID,cna=CNA \
-#                --id-property NUTS_ID \
-#                -e "shp/"$year"NUTS_AT_"$year"_prep.csv" \
-#                --bbox --width $size --height $size \
-#	            -s $s \
-#	            --q0 $q0 --q1 $q1
+            topojson -o \
+                $dir"/"$level".topojson" \
+                "nutsrg=tmp/"$year"/"$proj"/RGbylevel/RG_lvl"$level".shp" \
+                "nutsbn=tmp/"$year"/"$proj"/BNbylevel/BN_lvl"$level".shp" \
+                "cntrg=tmp/"$year"/"$proj"/CNTR_RG.shp" \
+                "cntbn=tmp/"$year"/"$proj"/CNTR_BN.shp" \
+                -p id=NUTS_ID,na=name,eu=EU_FLAG,efta=EFTA_FLAG,cc=CC_FLAG,lvl=LEVL_CODE,oth=OTHR_CNTR_,cid=CID,cna=CNA \
+                --id-property NUTS_ID \
+                -e "shp/"$year"NUTS_AT_"$year"_prep.csv" \
+                --bbox --width $size --height $size \
+	            -s $s \
+	            --q0 $q0 --q1 $q1
             fi
         done
     done
