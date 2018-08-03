448309f550dc5698784d66fbcc8c4d52f2105aef
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-03 16:16:01 +0200

    a

--- 3_shp_to_geojson.sh
+++ 3_shp_to_geojson.sh
@@ -9,9 +9,13 @@ do
 
       dir="tmp/"$year"/"$proj"/"$type
 
+      echo "$year $proj $type - country SHP to geojson"
+	  rm -f $dir"/CNTR.json"
+      ogr2ogr -overwrite -f geoJSON $dir"/CNTR.json" $dir"/CNTR.shp"
+
       for level in 0 1 2 3
       do
-        echo "$year $proj $level $type - SHP to geojson"
+        echo "$year $proj $type - NUTS $level SHP to geojson"
 		rm -f $dir"/"$level".json"
 	    ogr2ogr -overwrite -f geoJSON $dir"/"$level".json" $dir"/"$level".shp"
 
