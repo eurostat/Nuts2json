c968ba3d93cc55c4bbcefce969230fdc7c96b283
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-03 20:49:02 +0200

    a

--- 1_filter_project_clip.sh
+++ 1_filter_project_clip.sh
@@ -20,9 +20,9 @@ mkdir -p "tmp/"$year
 
 echo "$year Country RG: Clip and filter"
 ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
-   "tmp/"$year"/CNTR_RG_.shp" \
+   "tmp/"$year"/CNTR_RG.shp" \
    "shp/"$year"/CNTR_RG_01M_"$year".shp" \
-   -sql "SELECT CNTR_ID as id,SHRT_ENGL as na FROM CNTR_RG_01M_"$year" WHERE CNTR_ID NOT IN ('PT','ES','IE','UK','FR','IS','BE','LU','NL','CH','LI','DE','DK','IT','VA','MT','NO','SE','FI','EE','LV','LT','PL','CZ','SK','AT','SI','HU','HR','RO','BG','TR','EL','CY','MK','ME')" \
+   -sql "SELECT CNTR_ID as id,NAME_ENGL as na FROM CNTR_RG_01M_"$year" WHERE CNTR_ID NOT IN ('PT','ES','IE','UK','FR','IS','BE','LU','NL','CH','LI','DE','DK','IT','VA','MT','NO','SE','FI','EE','LV','LT','PL','CZ','SK','AT','SI','HU','HR','RO','BG','TR','EL','CY','MK','ME')" \
    -clipsrc -179 -89 179 89
 
 #necessary?
@@ -37,7 +37,7 @@ echo "$year Country BN: Clip and filter"
 ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/CNTR_BN.shp" \
    "shp/"$year"/CNTR_BN_01M_"$year".shp" \
-   -sql "SELECT CC_FLAG as cc,EFTA_FLAG as efta,EU_FLAG as eu,OTHR_FLAG as oth FROM CNTR_BN_01M_"$year" WHERE COAS_FLAG='F' AND OTHR_FLAG='T'" \
+   -sql "SELECT OTHR_FLAG as oth FROM CNTR_BN_01M_"$year" WHERE COAS_FLAG='F' AND OTHR_FLAG='T'" \
    -clipsrc -179 -89 179 89
 
 
@@ -52,7 +52,7 @@ echo "$year NUTS BN: Clip and filter"
 ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/NUTS_BN.shp" \
    "shp/"$year"/NUTS_BN_01M_"$year".shp" \
-   -sql "SELECT LEVL_CODE as lvl,EU_FLAG as eu,EFTA_FLAG as efta,CC_FLAG as cc,OTHR_CNTR_ as oth FROM NUTS_BN_01M_"$year \
+   -sql "SELECT LEVL_CODE as lvl,EU_FLAG as eu,EFTA_FLAG as efta,CC_FLAG as cc,OTHR_CNTR_ as oth,COAS_FLAG as coas FROM NUTS_BN_01M_"$year \
    -clipsrc -179 -89 179 89
 
 
