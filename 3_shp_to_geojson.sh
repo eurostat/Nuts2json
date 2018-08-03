c50037651b081c454471e288e89576344b23e38f
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-03 16:12:33 +0200

    a

--- 3_shp_to_geojson.sh
+++ 3_shp_to_geojson.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-#make topojson base files, one per nuts level
 for year in "2013" "2016"
 do
   for proj in "etrs89" "laea" "wm"
   do
-
-    dir="tmp/"$year"/"$proj"/"$type
-
-    for level in 0 1 2 3
+    for type in "RG" "BN"
     do
-      echo "$year $proj $level - geojson to topojson"
 
+      dir="tmp/"$year"/"$proj"/"$type
+
+      for level in 0 1 2 3
+      do
+        echo "$year $proj $level $type - SHP to geojson"
+		rm -f $dir"/"$level".json"
+	    ogr2ogr -overwrite -f geoJSON $dir"/"$level".json" $dir"/"$level".shp"
+
+      done
     done
   done
 done
--- 4_geojson_to_topojson.sh
+++ 4_geojson_to_topojson.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
+#make topojson base files, one per nuts level
+#https://github.com/topojson/topojson-server/blob/master/README.md#geo2topo
 for year in "2013" "2016"
 do
   for proj in "etrs89" "laea" "wm"
   do
-    for type in "RG" "BN"
+
+    dir="tmp/"$year"/"$proj
+
+    for level in 0 1 2 3
     do
+      echo "$year $proj $level - geojson to topojson"
+      geo2topo nutsrg=$dir"/RG/"$level".shp" > $dir"/"$level".topojson"
 
-      dir="tmp/"$year"/"$proj"/"$type
-
-      for level in 0 1 2 3
-      do
-        echo "$year $proj $level $type - SHP to geojson"
-		rm -f $dir"/"$level".json"
-	    ogr2ogr -overwrite -f geoJSON $dir"/"$level".json" $dir"/"$level".shp"
-
-      done
     done
   done
 done
