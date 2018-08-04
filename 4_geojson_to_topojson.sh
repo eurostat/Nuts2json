51ee21c4f88c54c0b35fe9b97073b67ce064220f
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-04 13:13:24 +0200

    new version with bug fixes

--- 4_geojson_to_topojson.sh
+++ 4_geojson_to_topojson.sh
@@ -4,13 +4,13 @@
 #https://github.com/topojson/topojson-server/blob/master/README.md#geo2topo
 for year in "2013" "2016"
 do
-  for proj in "laea" "etrs89" "laea" "wm"
+  for proj in "etrs89" "laea" "wm"
   do
     dir="tmp/"$year"/"$proj
     for level in 0 1 2 3
     do
       echo "$year $proj $level - geojson to topojson"
-      geo2topo -q 20000 nutsrg=$dir"/RG/"$level".json" nutsbn=$dir"/BN/"$level".json" cntrrg=$dir"/RG/CNTR.json" cntrbn=$dir"/BN/CNTR.json" > $dir"/"$level".topojson"
+      geo2topo -q 20000 nutsrg=$dir"/RG/"$level".json" nutsbn=$dir"/BN/"$level".json" cntrg=$dir"/RG/CNTR.json" cntbn=$dir"/BN/CNTR.json" > $dir"/"$level".topojson"
       #quantization: q small means strong simplification
     done
   done
--- 5_simplify_topojson.sh
+++ 5_simplify_topojson.sh
@@ -4,7 +4,7 @@
 #https://github.com/topojson/topojson-simplify/blob/master/README.md#toposimplify
 for year in "2016" #"2013"
 do
-  for proj in "laea" "etrs89" "laea" "wm"
+  for proj in "etrs89" "laea" "wm"
   do
     dir="tmp/"$year"/"$proj
     for level in 0 1 2 3
