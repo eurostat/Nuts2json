6f913a18e8926355ede901963c10ca647723e36a
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-06 10:30:45 +0200

    a

--- 4_geojson_to_topojson.sh
+++ 4_geojson_to_topojson.sh
@@ -11,7 +11,7 @@ do
     do
       echo "$year $proj $level - geojson to topojson"
       geo2topo -q 20000 nutsrg=$dir"/RG/"$level".json" nutsbn=$dir"/BN/"$level".json" cntrg=$dir"/RG/CNTR.json" cntbn=$dir"/BN/CNTR.json" > $dir"/"$level".json"
-      #quantization: q small means strong simplification
+      #quantization: q small means strong 'simplification'
     done
   done
 done
