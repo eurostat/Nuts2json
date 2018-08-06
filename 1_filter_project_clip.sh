7d5c96f5e2af29a4b2e47dde7f7aa4b936a3244c
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-06 15:35:44 +0200

    a

--- 1_filter_project_clip.sh
+++ 1_filter_project_clip.sh
@@ -20,7 +20,7 @@ ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/CNTR_RG.shp" \
    "shp/"$year"/CNTR_RG_01M_"$year".shp" \
    -sql "SELECT CNTR_ID as id,NAME_ENGL as na FROM CNTR_RG_01M_"$year" WHERE CNTR_ID NOT IN ('PT','ES','IE','UK','FR','IS','BE','LU','NL','CH','LI','DE','DK','IT','VA','MT','NO','SE','FI','EE','LV','LT','PL','CZ','SK','AT','SI','HU','HR','RO','BG','TR','EL','CY','MK','ME')" \
-   -clipsrc -40.1 25.1 70 89.1
+   -clipsrc -120 25.1 120 89.1
 
 #necessary?
 #echo "$year Country RG: Join attributes"
@@ -35,7 +35,7 @@ ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/CNTR_BN.shp" \
    "shp/"$year"/CNTR_BN_01M_"$year".shp" \
    -sql "SELECT OTHR_FLAG as oth FROM CNTR_BN_01M_"$year" WHERE COAS_FLAG='F' AND OTHR_FLAG='T'" \
-   -clipsrc -40.1 25.1 70 89.1
+   -clipsrc -120 25.1 120 89.1
 
 
 echo "1- $year NUTS RG: Clip and filter"
@@ -43,14 +43,14 @@ ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/NUTS_RG.shp" \
    "shp/"$year"/NUTS_RG_01M_"$year".shp" \
    -sql "SELECT NUTS_ID as id,NUTS_NAME as na,LEVL_CODE as lvl FROM NUTS_RG_01M_"$year \
-   -clipsrc -40.1 25.1 70 89.1
+   -clipsrc -120 25.1 120 89.1
 
 echo "1- $year NUTS BN: Clip and filter"
 ogr2ogr -overwrite -f "ESRI Shapefile" -lco ENCODING=UTF-8 \
    "tmp/"$year"/NUTS_BN.shp" \
    "shp/"$year"/NUTS_BN_01M_"$year".shp" \
    -sql "SELECT LEVL_CODE as lvl,EU_FLAG as eu,EFTA_FLAG as efta,CC_FLAG as cc,OTHR_CNTR_ as oth,COAS_FLAG as coas FROM NUTS_BN_01M_"$year \
-   -clipsrc -40.1 25.1 70 89.1
+   -clipsrc -120 25.1 120 89.1
 
 
   for pi in ${!projs[@]}
