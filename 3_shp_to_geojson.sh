99b3da84926586dd110ddd45d4ed556187dccc88
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-07 09:34:22 +0200

    a

--- 3_shp_to_geojson.sh
+++ 3_shp_to_geojson.sh
@@ -4,6 +4,11 @@ for year in "2016" "2013" "2010"
 do
   for proj in "3035" "3857" "4258"
   do
+
+    echo "3- $year $proj - graticule SHP to geojson"
+    rm -f "tmp/"$year"/"$proj"/graticule.json"
+    ogr2ogr -overwrite -f geoJSON "tmp/"$year"/"$proj"/graticule.json" "tmp/$year/$proj/graticule.shp"
+
     for type in "RG" "BN"
     do
 
