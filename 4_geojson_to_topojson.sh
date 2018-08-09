e871f1740cf21b824e65c967455247fd459282c8
julien Gaffuri <julien.gaffuri@gmail.com>
2018-08-09 12:04:46 +0200

    a

--- 4_geojson_to_topojson.sh
+++ 4_geojson_to_topojson.sh
@@ -4,14 +4,17 @@
 #https://github.com/topojson/topojson-server/blob/master/README.md#geo2topo
 for year in "2016" "2013" "2010"
 do
+for scale in "01"
+do
   for proj in "3035" "3857" "4258"
   do
-    dir="tmp/"$year"/"$proj
+    dir="tmp/$year/$scale/$proj"
     for level in 0 1 2 3
     do
-      echo "4- $year $proj $level - geojson to topojson"
+      echo "4- $year $scale $proj $level - geojson to topojson"
       geo2topo -q 20000 nutsrg=$dir"/RG/"$level".json" nutsbn=$dir"/BN/"$level".json" cntrg=$dir"/RG/CNTR.json" cntbn=$dir"/BN/CNTR.json" gra=$dir"/graticule.json" > $dir"/"$level".json"
       #quantization: q small means strong 'simplification'
     done
   done
+done
 done
--- 6_topojson_to_geojson.sh
+++ 6_topojson_to_geojson.sh
@@ -4,19 +4,19 @@
 #https://github.com/topojson/topojson-client/blob/master/README.md#topo2geo
 for year in "2016" "2013" "2010"
 do
+for scale in "01"
+do
   for proj in "3035" "3857" "4258"
   do
-    dir="tmp/"$year"/"$proj
+    dir="tmp/$year/$scale/$proj"
     for level in 0 1 2 3
     do
-      for size in 1200 1000 800 600 400
-      do
-        outdir=$year"/"$proj"/"$size"px"
+        outdir=$year"/"$proj"/"$scale"M"
         mkdir -p $outdir
 
-        echo "6- $year $proj $level $size - topojson to geojson"
+        echo "6- $year $scale $proj $level $size - topojson to geojson"
         topo2geo nutsrg=$outdir"/nutsrg_"$level".json" nutsbn=$outdir"/nutsbn_"$level".json" cntrg=$outdir"/cntrg.json" cntbn=$outdir"/cntbn.json" gra=$outdir"/gra.json" < $outdir"/"$level".json"
-      done
     done
   done
+done
 done
