5be8d987a35946c18efb68145f374e77ab602255
jgaffuri <jgaf@protonmail.com>
2021-09-21 16:08:55 +0200

    precision reducer

--- src/py/reduceGeoJSON.py
+++ src/py/reduceGeoJSON.py
@@ -10,23 +10,46 @@ def reduceFile(filePath):
 
 def reduceGeoJSON(data):
     type = data["type"]
-    if(type == "FeatureCollection"): return reduceFeatureCollection(data)
-    if(type == "Feature"): return reduceFeature(data)
-    if(type == "Geometry"): return reduceGeometry(data)
+    if(type == "FeatureCollection"): reduceFeatureCollection(data)
+    if(type == "Feature"): reduceFeature(data)
+    if(type == "Geometry"): reduceGeometry(data)
+
 
 
 
 def reduceFeatureCollection(fc):
-    print(fc)
-    #aa
+    for i in range(len(fc["features"])):
+        f = fc["features"][i]
+        reduceFeature(f)
+        fc["features"][i] = f
 
 def reduceFeature(f):
-    print(f)
-    ##
+    g = f["geometry"]
+    reduceGeometry(g)
+    f["geometry"] = g
 
 def reduceGeometry(g):
-    print(g)
-    ##
+    type = g["type"]
+    if(type == "LineString"): reduceLineString(g)
+    else: print("Not supported geometry type: " + type)
+
+def reduceLineString(ls):
+    cs = ls["coordinates"]
+    reduceCoordinates(cs)
+    ls["coordinates"] = cs
+
+def reduceCoordinates(cs):
+    for i in range(len(cs)):
+
+#2.1.1. Positions
+#2.1.2. Point
+#2.1.3. MultiPoint
+#2.1.4. 
+#2.1.5. MultiLineString
+#2.1.6. Polygon
+#2.1.7. MultiPolygon
+#2.1.8 Geometry Collection
+
 
 
 reduceFile("pub/v2/2021/3035/03M/nutsbn_0.json")
